61c44820ddc6027c3b81bd76239721c526b62ccf
brenohof <brenohenriqueof@gmail.com>
2021-06-27 15:52:11 -0300

    setup projeto

---
+++ src/test/java/br/com/zup/brenohof/servicetransacao/ServiceTransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.brenohof.servicetransacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceTransacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
